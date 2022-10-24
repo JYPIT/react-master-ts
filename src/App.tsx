647d33b0417aff56055d95a84c505cf13946f4f1
JYP <jypit@naver.com>
2022-10-24 10:23:57 +0900

    mackbook test

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
+
+const GlobalStyle = createGlobalStyle`
+body{
+  color:red;
+}`;
+
+function App() {
+  return <Router />;
+}
+
+export default App;
